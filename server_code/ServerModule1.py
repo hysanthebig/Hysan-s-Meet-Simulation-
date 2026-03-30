4bc46cb3294a069045238fe8b37cd1dd923b4046
hysan110204@gmail.com <hysan110204@gmail.com>
2026-03-30 16:00:45 +0000

    Created csv_less_data_loader; Edited settings, ServerModule1 and Form1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -25,7 +25,15 @@ def tabler(rows):
       "Avr_splits":r['Avr_splits']
     })
   df = pd.DataFrame(data_list)
+  return df
 
+def table_into_df(sport):
+  if sport == "XC":
+    rows = app_tables.datatable.search()
+  if sport == "Track":
+    rows = app_tables.track_table.search()
+  return tabler(rows)
+  
 def seconds_to_mintunes(seconds):
   mint = int(seconds // 60)
   sec = round(seconds % 60, 1)
@@ -40,3 +48,52 @@ def time_to_seconds(time):
   seconds = float(seconds)
   time_seconds = mintunes*60 + seconds
   return time_seconds
+
+
+@anvil.server.callable
+def filter(sport,sort_by,runnerlist,racelist,gradelist,lengthlist):
+  start = time.time()
+  df = table_into_df(sport)
+
+  readmask = pd.Series(True, index=df.index)
+  runner_mask = pd.Series(False, index=df.index)
+  race_mask = pd.Series(False, index=df.index)
+  grade_mask = pd.Series(False, index = df.index)
+  length_mask = pd.Series(False,index = df.index)
+  ####################Filter#######################
+  runner_mask = df["Runner"].str.lower().isin([r.lower() for r in racelist])
+  if len(runnerlist) == 0:
+    runner_mask = pd.Series(True,index =df.index)
+
+  race_mask = df['Race'].str.lower().isin([r.lower() for r in racelist])
+  if len(racelist) == 0:
+    race_mask = pd.Series(True,index =df.index)
+
+  grade_mask = df['Grade'].astype(str).isin([r.lower() for r in gradelist])
+  if len(gradelist) == 0:
+    grade_mask = pd.Series(True,index =df.index)
+
+  length_mask = df['Length'].str.lower().isin([r.lower() for r in lengthlist])
+  if len(lengthlist) == 0:
+    length_mask = pd.Series(True,index =df.index)
+
+  readmask = readmask & runner_mask & race_mask & grade_mask & length_mask
+
+  df_filtered = df.loc[readmask]
+  df_filtered = df_filtered.sort_values(by=[sort_by])
+  df_filtered =df_filtered.to_dict(orient="records")
+
+
+  end = time.time()
+  print(f"filter {end-start:.4f}")
+
+  return(df_filtered)
+
+def pr_display(sport,runnerlist,lengthlist,gradelist):
+  filitered_df = filter(sport,"Runner",runnerlist,[],gradelist,lengthlist)
+  df_pr = tabler(filitered_df)
+  df_pr = df_pr.sort_values(by = ["time_seconds"])
+  pr_df = df_pr.groupby("Runner")['time_seconds'].min().copy()
+  pr_rows = df_pr[df_pr["time_seconds"] == df_pr["Runner"].map(pr_df)]
+  pr_rows = pr_rows.drop(columns = ['time_seconds','Date_dt']).to_dict(orient="records")
+  return(pr_rows)
