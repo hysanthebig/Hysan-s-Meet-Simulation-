aa5e0df6c5891dde8d706e097a38e7f2f739dd29
hysan110204@gmail.com <hysan110204@gmail.com>
2026-04-21 02:04:22 +0000

    Created Form4 and Form4.ItemTemplate2; Deleted Form1.RowTemplate1.RowTemplate4, Form1.RowTemplate2, Form1.RowTemplate1 and Form1.ItemTemplate2

--- client_code/Form4/__init__.py
+++ client_code/Form4/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import RowTemplate4Template
+from ._anvil_designer import Form4Template
 from anvil import *
 import anvil.server
 import m3.components as m3
@@ -7,7 +7,7 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class RowTemplate4(RowTemplate4Template):
+class Form4(Form4Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
