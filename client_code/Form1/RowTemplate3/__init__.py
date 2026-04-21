96c20bb7e8996663447c2706c6d57483409596bb
hysan110204@gmail.com <hysan110204@gmail.com>
2026-04-21 02:47:44 +0000

    Created Form1.RowTemplate1, Form1.ItemTemplate6 and Form1.ItemTemplate5; Edited Form1.RowTemplate3

--- client_code/Form1/RowTemplate3/__init__.py
+++ client_code/Form1/RowTemplate3/__init__.py
@@ -9,8 +9,26 @@ from anvil.tables import app_tables
 
 class RowTemplate3(RowTemplate3Template):
   def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.text_box_time.text = self.item["Time"]
 
-    # Any code you write here will run before the form opens.
+    # Fill UI with row data
+    self.text_1.text = self.item.get("Rank", "")
+    self.text_2.text = self.item.get("School", "")
+    self.text_3.text = self.item.get("Runner", "")
+    self.text_4.text = self.item.get("Grade", "")
+    self.text_5.text = self.item.get("Length", "")
+    self.text_box_1.text = self.item.get("Time", "")
+    self.text_6.text = self.item.get("Points", "")
+
+  def text_box_time_change(self, **event_args):
+    new_time = self.text_box_time.text
+    self.item["Time"] = new_time
+
+    # optional: update seconds too
+    try:
+      self.item["time_seconds"] = anvil.server.call("time_to_seconds", new_time)
+    except:
+      pass
+
+    # tell parent something changed
+    self.parent.raise_event("x-refresh")
