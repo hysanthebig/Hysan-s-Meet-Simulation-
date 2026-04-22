bb48e9162eb236efd5da9ba2defd6a69bff19fbd
hysan110204@gmail.com <hysan110204@gmail.com>
2026-04-22 02:34:35 +0000

    Edited Form1.RowTemplate2 and Form1

--- client_code/Form1/RowTemplate2/__init__.py
+++ client_code/Form1/RowTemplate2/__init__.py
@@ -12,12 +12,13 @@ class RowTemplate2(RowTemplate2Template):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     self.time_box.text = self.item['Time']
-    self.time_box.set_event_handler('pressed_enter', self.time_box_enter)
+    self.time_box.set_event_handler('lost_focus', self.time_box_enter)
     
 
     # Any code you write here will run before the form opens.
 
   def time_box_enter(self, **event_args):
     print(self.item["Time"])
-    
+    print(self.item["Runner"])
+    print(self.item["Length"])
 
